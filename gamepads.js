00148e669fec7145d85db0fa0d704110664af1b4
reona396 <chihiro396@gmail.com>
2018-10-08 01:00:57 +0900

    ボタン名と押した時のアクション追加

--- gamepads.js
+++ gamepads.js
@@ -26,45 +26,64 @@ const buttonMapping = {
 }
 
 function setup() {
-  createCanvas(400,650);
+  createCanvas(400,670);
+  textAlign(LEFT,CENTER);
 }
 
 
 function draw() {
   background(200);
 
-  fill(0);
-  for(let p = 0; p < buttonNum; p++) {
-    const name = buttonMapping[p]buttonNum;
-      text(name, 80, 20 * p);
+  let buttonCount = 0;
+  for(let p = 0; p <= buttonNum; p++) {
+    const name = buttonMapping[p] || p;
+    if(isNaN(name)){
+      fill(0);
+      text(name, 100, 25 * buttonCount + 30);
+      noFill();
+      ellipse(85, 25 * buttonCount + 30, 13, 13);
+      buttonCount++;
+    }
   }
 
-  // let gamepads = navigator.getGamepads ? navigator.getGamepads() : [];
-  // let gamepadArray = [];
-  // for(let i = 0; i < gamepads.length; i++) {
-  //   gamepadArray.push(gamepads[i]);
-  // }
-  // let orderedGamepads = [];
-  // orderedGamepads.push(gamepadArray.find(g => g.id.indexOf('Joy-Con (R)') > -1));
-  // orderedGamepads.push(gamepadArray.find(g => g.id.indexOf('Joy-Con (L)') > -1));
-  // let pressed = [];
-  //
-  //   for (let g = 0; g < orderedGamepads.length; g++) {
-  //       const gp = orderedGamepads[g];
-  //       if (!!gp) {
-  //
-  //           for(let i = 0; i < gp.buttons.length; i++) {
-  //               if(gp.buttons[i].pressed) {
-  //                   const id = (g * 15) + i + g;
-  //                   const button = buttonMapping[id] || id;
-  //                   // pressed.push(button);
-  //                   text(button, width/2,height/2);
-  //               }
-  //
-  //           }
-  //
-  //
-  //       }
-  //   }
+  let gamepads = navigator.getGamepads ? navigator.getGamepads() : [];
+  let gamepadArray = [];
+  for(let i = 0; i < gamepads.length; i++) {
+    gamepadArray.push(gamepads[i]);
+  }
+  let orderedGamepads = [];
+  orderedGamepads.push(gamepadArray.find(g => g.id.indexOf('Joy-Con (R)') > -1));
+  orderedGamepads.push(gamepadArray.find(g => g.id.indexOf('Joy-Con (L)') > -1));
+  let pressed = [];
 
+    for (let g = 0; g < orderedGamepads.length; g++) {
+        const gp = orderedGamepads[g];
+        if (!!gp) {
+
+            for(let i = 0; i < gp.buttons.length; i++) {
+
+                if(gp.buttons[i].pressed) {
+                    const pressedId = (g * 15) + i + g;
+                    const pressedIndex = getIndex(pressedId);;
+                    // const button = buttonMapping[id] || id;
+                    // pressed.push(button);
+                    
+                    fill(200, 0, 0);
+                    ellipse(85, 25 * pressedIndex + 30, 13, 13);
+                }
+            }
+        }
+    }
+}
+
+function getIndex(id) {
+  var resultIndex = 0
+  for(let i = 0; i < id; i++) {
+    const name = buttonMapping[i] || i;
+    if(isNaN(name)){
+      resultIndex++;
+    }
+  }
+
+  return resultIndex;
 }
